ba172f20423d4ca3f879c0e89ff88dc5b50c4494
Yusuf-wahyu <Yusuf-wahyu>
2022-10-08 23:54:42 +0700

    menyelesaikan video ke 6 tentang menambahkan assets

--- phpmvc/app/views/about/index.php
+++ phpmvc/app/views/about/index.php
@@ -1,6 +1,7 @@
-
-    <h1>about me</h1>
+<div class="container">
+    <h1 class ="mt-4">about me</h1>
+    <img src="<?= BASEURL; ?>/img/ucup.jpeg" alt="Yusuf wahyu syahroni" width = "200" class="rounded-circle shadow">
     <p> Halo, nama saya <?= $data['nama']; ?> 
     umur saya <?= $data['umur'];?> 
     tahun dan saya adalah seorang <?= $data['pekerjaan'];?>.</p>
-
+    </div>
