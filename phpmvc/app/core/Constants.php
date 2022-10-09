235a65d8563019212909e1faa95abb1c6d0462b1
Yusuf-wahyu <Yusuf-wahyu>
2022-10-09 14:24:22 +0700

    menyelesaikan video ke 8 tentang database wrapper

--- phpmvc/app/core/Constants.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-define ('BASEURL', 'http//localhost/prakweb_a_203040052/phpmvc/public');
--- phpmvc/app/models/Mahasiswa_model.php
+++ phpmvc/app/models/Mahasiswa_model.php
@@ -1,21 +1,15 @@
 <?php
 
 class Mahasiswa_model{
-    //database handler
-    private $dbh,
-    private $stmt;
+private $table = 'mahasiswa';
+private $db;
 
-    public function __construct()
-    {
-        //data source name
-        $dsn ='mysql:host=localhost;dbname=phpmvc';
+public function __construct()
+{
+    $this->db = new Database;
+}
 
-        try{
-            $this->dbh = new PDO($dsn, 'root', '');
-        } catch (PDOException $e) {
-            die($e->getMessage());
-        }
-    }
+
     // private $mhs = [
     // [
     //     "nama"=> "Yusuf Wahyu Syahroni",
@@ -39,9 +33,15 @@ class Mahasiswa_model{
 
     public function getAllMahasiswa()
         {
-            $this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
-            $this->stmt->execute();
-            return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+            $this->db->query('SELECT * FROM ' . $this->table);
+            return $this->db->resultSet();
+        }
+
+        public function getMahasiswaById($id)
+        {
+            $this->db->query('SELECT *FROM' . $this->table . 'WHERE id=:id');
+            $this->db->db->bind('id', $id);
+            return $this->db->single();
         }
     
 }
